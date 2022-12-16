04a43cb661324ead154fead84669709f1228b120
RenZichang <33561455+RenZichang@users.noreply.github.com>
2022-12-16 16:56:23 +0800

    Update softDecode.m
    修改了一个硝啸的bug

--- private/softDecode.m
+++ private/softDecode.m
@@ -18,7 +18,7 @@ while (sum(mod(H * L_V.', 2)) ~= 0) && (iter_time < 30)
         j = ceil(edge_inds(iter_edge) / N_K);
         i = mod(edge_inds(iter_edge) - 1, N_K) + 1;
         % 似然更新
-        L_V2F(j, i) = L_V(j) + sum(L_F2V(1:end~=i, j), 'all');
+        L_V2F(j, i) = c(j) + sum(L_F2V(1:end~=i, j), 'all');
     end
     % 从校验到变量的似然更新，只遍历存在的边
     for iter_edge = 1:numel(edge_inds)
@@ -29,9 +29,7 @@ while (sum(mod(H * L_V.', 2)) ~= 0) && (iter_time < 30)
         L_F2V(i, j) = 2 * atanh(prod(tanh(L_V2F(H(i, 1:end)==1, i) / 2), 'all') / tanh(L_V2F(j, i) / 2));
     end
     % 计算变量节点置信度
-    for j = 1:N
-        L_V(j) = L_V(j) + sum(L_F2V(:, j), 'all');
-    end
+    L_V = c + sum(L_F2V, 1);
 end
 dc = (1 - sign(L_V)) / 2;
 end
